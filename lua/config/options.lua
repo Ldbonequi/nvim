bc1e8632686706645b871e29aac4679d37050b71
Ldbonequi <ldbonequi@gmail.com>
2025-01-25 20:49:10 -0700

    added highlight yank, show tabs

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,6 +7,10 @@ vim.opt.wrap = false -- display lines as single line
 vim.opt.scrolloff = 10 -- number of lines to keep above/below cursor
 vim.opt.sidescrolloff = 8 -- number of columns to keep to the left/right of cursor
 
+--show tabs
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
 -- Tab spacing/behavior
 vim.opt.expandtab = true -- convert tabs to spaces
 vim.opt.shiftwidth = 4 -- number of spaces inserted for each indentation level
@@ -14,6 +18,15 @@ vim.opt.tabstop = 4 -- number of spaces inserted for tab character
 vim.opt.softtabstop = 4 -- number of spaces inserted for <Tab> key
 vim.opt.smartindent = true -- enable smart indentation
 vim.opt.breakindent = true -- enable line breaking indentation
+
+-- highlight when yanking
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
 
 -- General Behaviors
 vim.g.loaded_netrw = 1
