db76d25c8119d2899e5ca78cf9e943226121901a
ldbonequi <ldbonequi@gmail.com>
2025-02-27 12:18:25 -0700

    added leap and . repeat

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,10 @@
 -- Set our leader keybinding to space
 -- Anywhere you see <leader> in a keymapping specifies the space key
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+
+-- Map space to WhichKey in normal and visual modes using VSCode command
+vim.keymap.set({ "n", "v" }, "<Space>", function()
+    require('vscode').call('whichkey.show')
+end, { silent = true, desc = "Show WhichKey" })
 
 -- Remove search highlights after searching
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Remove search highlights" })
@@ -18,4 +21,3 @@ vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" }
 -- Stay in indent mode
 vim.keymap.set("v", "<", "<gv", { desc = "Indent left in visual mode" })
 vim.keymap.set("v", ">", ">gv", { desc = "Indent right in visual mode" })
-
---
+++ lua/plugins/vim-repeat.lua
@@ -0,0 +1,4 @@
+return {
+  "tpope/vim-repeat"
+  -- to enable in any plugin add at end of map: silent! call repeat#set("\<Plug>MyWonderfulMap", v:count)
+}
